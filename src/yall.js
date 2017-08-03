8eb24f48839a2922ca2e5bc88b52f57f80037337
Kamran Ayub <kamran.ayub@gmail.com>
2017-08-02 20:09:31 -0500

    961 bytes

--- src/yall.js
+++ src/yall.js
@@ -7,86 +7,91 @@
  **/
 
 ((window, document)=>{
-	let yall = {
-		/** Object members are defined here.
-		  * Anything suffixed with a double asterisk (**)
-		  * indicates that it was defined solely to shave
-		  * bytes from the final build.
-		  **/
-
-		// Tracks if yall is working. Used for throttling.
-		a: false,
-
-		// A multiple event binding handler.**
-		b: (obj, handlers, func, add)=>{
-			handlers.split(" ").forEach((handler)=>{
-				add ? obj.addEventListener(handler, func) : obj.removeEventListener(handler, func);
-			});
-		},
-
+	/** 
+	 * Anything suffixed with a double asterisk (**)
+	 * indicates that it was defined solely to shave
+	 * bytes from the final build.
+	 **/
+		
+	const 
+		// Placeholders used common method names.**
+		qsa = "querySelectorAll",
+	    sa = "setAttribute",
+		ga = "getAttribute",
+		ra = "removeAttribute",
+		ael = "addEventListener",
+		rel = "removeEventListener",
 		// Placeholders used for "data-src" and "data-srcset" attribute references.**
-		s: "data-src",
-		ss: "data-srcset",
-
+		s = "data-src",
+		ss = "data-srcset",
 		// Placeholders used for event handler strings.**
-		y: "scroll touchmove",
-		z: "orientationchange resize",
+		y = "scroll touchmove",
+		z = "orientationchange resize";
 
-		// The guts of the lazy loader
-		g: ()=>{
-			if(!document.querySelectorAll(".lazy").length){
-				yall.b(document, yall.y, yall.g);
-				yall.b(window, yall.z, yall.g);
-				return;
-			}
+	// Tracks if yall is working. Used for throttling.
+	let a = false;
 
-			if(!yall.a){
-				yall.a = true;
-
-				setTimeout(()=>{
-					yall.i.forEach((img)=>{
-						if(img.getBoundingClientRect().top <= ((document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight + 100) && getComputedStyle(img, null).display != "none"){
-							if(img.parentNode.tagName == "PICTURE"){
-								let sources = [].slice.call(img.parentNode.querySelectorAll("source"));
-
-								sources.forEach((source)=>{
-									let sourceSrcset = source.getAttribute(yall.ss);
-
-									if(sourceSrcset != null){
-										source.setAttribute("srcset", sourceSrcset);
-										source.removeAttribute(yall.ss);
-									}
-								});
-							}
-
-							let imgSrc = img.getAttribute(yall.s);
-							let imgSrcset = img.getAttribute(yall.ss);
-
-							if(imgSrc != null){
-								img.src = imgSrc;
-								img.removeAttribute(yall.s);
-							}
-
-							if(imgSrcset != null){
-								img.srcset = imgSrcset;
-								img.removeAttribute(yall.ss);
-							}
-
-							img.classList.remove("lazy");
-						}
-					});
-
-					yall.a = false;
-				}, 200);
-			}
-		}
+	// A multiple event binding handler.**
+	let b = (obj, handlers, func, add)=>{
+		handlers.split(" ").forEach((handler)=>{
+			add ? obj[ael](handler, func) : obj[rel](handler, func);
+		});
 	};
 
+	// The guts of the lazy loader
+	let yall = ()=>{
+		if(!document[qsa](".lazy").length){
+			b(document, y, yall);
+			b(window, z, yall);
+			return;
+		}
+
+		if(!a){
+			a = true;
+
+			setTimeout(()=>{
+				yall.i.forEach((img)=>{
+					if(img.getBoundingClientRect().top <= ((document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight + 100) && getComputedStyle(img, null).display != "none"){
+						if(img.parentNode.tagName == "PICTURE"){
+							let sources = [].slice.call(img.parentNode[qsa]("source"));
+
+							sources.forEach((source)=>{
+								let sourceSrcset = source[ga](ss);
+
+								if(sourceSrcset != null){
+									source[sa]("srcset", sourceSrcset);
+									source[ra](ss);
+								}
+							});
+						}
+
+						let imgSrc = img[ga](s);
+						let imgSrcset = img[ga](ss);
+
+						if(imgSrc != null){
+							img.src = imgSrc;
+							img[ra](s);
+						}
+
+						if(imgSrcset != null){
+							img.srcset = imgSrcset;
+							img[ra](ss);
+						}
+
+						img.classList.remove("lazy");
+					}
+				});
+
+				a = false;
+			}, 200);
+		}
+	};	
+
 	// Everything's kicked off on DOMContentLoaded
-	yall.b(document, "DOMContentLoaded", ()=>{
-		yall.i = [].slice.call(document.querySelectorAll(".lazy"));
-		yall.g();
-		yall.b(document, yall.y, yall.g, true);
-		yall.b(window, yall.z, yall.g, true);
+	b(document, "DOMContentLoaded", ()=>{
+		yall.i = [].slice.call(document[qsa](".lazy"));
+		yall();
+		b(document, y, yall, true);
+		b(window, z, yall, true);
 	}, true);
 })(window, document);
