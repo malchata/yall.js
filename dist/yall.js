09e5a2e5adfac1a35bb28741593d0e6e993a514d
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2020-04-03 11:37:10 -0500

    Tweaks to lower size, fix IE11 autoplay issue.

--- dist/yall.js
+++ dist/yall.js
@@ -53,15 +53,24 @@ function yall (options) {
   };
 
   // Added because there was a number of patterns like this peppered throughout
-  // the code. This just flips necessary data- attrs on an element
+  // the code. This flips necessary data- attrs on an element and prompts video
+  // elements to begin playback automatically if they have autoplay specified.
   const yallFlipDataAttrs = element => {
     for (let dataAttrIndex in dataAttrs) {
       if (dataAttrs[dataAttrIndex] in element.dataset) {
         element.setAttribute(dataAttrs[dataAttrIndex], element.dataset[dataAttrs[dataAttrIndex]]);
+        const parentNode = element.parentNode;
 
-        if (element.nodeName === "SOURCE" && element.parentNode.autoplay) {
-          element.parentNode.load();
-          element.parentNode.classList.remove(lazyClass);
+        if (element.nodeName === "SOURCE" && parentNode.autoplay) {
+          parentNode.load();
+
+          // For some reason, IE11 needs to have this method invoked in order
+          // for autoplay to start. So we do a yucky user agent check.
+          if (/Trident/.test(navigator.userAgent)) {
+            parentNode.play();
+          }
+
+          parentNode.classList.remove(lazyClass);
         }
 
         element.classList.remove(lazyClass);
@@ -77,45 +86,20 @@ function yall (options) {
     }
   };
 
-  const yallIntersectionObserve = entry => {
-    if (entry.isIntersecting || entry.intersectionRatio) {
-      const element = entry.target;
-
-      if (ric in win && idleLoadTimeout) {
-        win[ric](() => {
-          yallLoad(element);
-        }, {
-          timeout: idleLoadTimeout
-        });
-      } else {
-        yallLoad(element);
-      }
-
-      intersectionListener.unobserve(element);
-      lazyElements = lazyElements.filter(lazyElement => lazyElement != element);
-
-      if (!lazyElements.length && !observeChanges) {
-        intersectionListener.disconnect();
-      }
-    }
-  };
-
-  const yallMutationObserve = newElement => {
-    if (lazyElements.indexOf(newElement) < 0) {
-      lazyElements.push(newElement);
-      yallBindEvents(newElement);
-
-      if (ioSupport && !crawler) {
-        intersectionListener.observe(newElement);
-      } else if (noPolyfill || crawler) {
-        yallApplyFn(lazyElements, yallLoad);
-      }
-    }
-  };
-
   const yallCreateMutationObserver = entry => {
     new MutationObserver(() => {
-      yallApplyFn(queryDOM(), yallMutationObserve);
+      yallApplyFn(queryDOM(), newElement => {
+        if (lazyElements.indexOf(newElement) < 0) {
+          lazyElements.push(newElement);
+          yallBindEvents(newElement);
+
+          if (ioSupport && !crawler) {
+            intersectionListener.observe(newElement);
+          } else if (noPolyfill || crawler) {
+            yallApplyFn(lazyElements, yallLoad);
+          }
+        }
+      });
     }).observe(entry, options.mutationObserverOptions || {
       childList: true,
       subtree: true
@@ -124,23 +108,47 @@ function yall (options) {
 
   let lazyElements = queryDOM();
 
+  yallApplyFn(lazyElements, yallBindEvents);
+
   // First we check if IntersectionObserver is supported. If not, we check to
-  // see if the `noPolyfill` option is set. If so, we load everything.
+  // see if the `noPolyfill` option is set. If so, we load everything. If the
+  // current user agent is a known crawler, again, we load everything.
   if (ioSupport && !crawler) {
     var intersectionListener = new win[io](entries => {
-      yallApplyFn(entries, yallIntersectionObserve);
+      yallApplyFn(entries, entry => {
+        if (entry.isIntersecting || entry.intersectionRatio) {
+          const element = entry.target;
+
+          if (ric in win && idleLoadTimeout) {
+            win[ric](() => {
+              yallLoad(element);
+            }, {
+              timeout: idleLoadTimeout
+            });
+          } else {
+            yallLoad(element);
+          }
+
+          intersectionListener.unobserve(element);
+          lazyElements = lazyElements.filter(lazyElement => lazyElement != element);
+
+          // If all the elements that were detected at load time are all loaded
+          // and we're not observing for changes, we're all done here.
+          if (!lazyElements.length && !observeChanges) {
+            intersectionListener.disconnect();
+          }
+        }
+      });
     }, {
       rootMargin: `${"threshold" in options ? options.threshold : 200}px 0%`
     });
 
-    yallApplyFn(lazyElements, yallBindEvents);
     yallApplyFn(lazyElements, intersectionListener);
 
     if (observeChanges) {
       yallApplyFn(queryDOM(options.observeRootSelector || "body"), yallCreateMutationObserver);
     }
   } else if (noPolyfill || crawler) {
-    yallApplyFn(lazyElements, yallBindEvents);
     yallApplyFn(lazyElements, yallLoad);
   }
 }
